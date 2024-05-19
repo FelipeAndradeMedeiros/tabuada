6731688463cf41f9887333de1d2b3b29fa90f4c7
Felipe Andrade <felipeandrademedeiros666@gmail.com>
2024-05-19 16:18:23 -0300

    tabuada

---
+++ script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+    let num =document.getElementById('txtn')
+    let tab =document.getElementById('seltab')
+    if (num.value == 0){
+       window.alert('DIgite um numero')
+    } else {
+        n = Number(num.value)
+        c = 1
+        tab.innerHTML = ""
+        while( c <= 200){
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+        
+    } 
+    
+}
